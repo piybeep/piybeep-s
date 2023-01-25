0d87e94958aa9ab55b4e3c86e3476346bdb4d6c3
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-01-25 13:49:42 +0300

    Reviews (#2)
    * feat: findall и findbyid
    
    * feat: отзывы + логирование
    
    Signed-off-by: Даня <45875910+danyatochkaru@users.noreply.github.com>

---
+++ src/reviews/dto/create-review.dto.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateReviewDto {
+	@ApiProperty()
+	@IsString()
+	@IsNotEmpty()
+	author: string;
+
+	@ApiProperty()
+	@IsString()
+	@IsNotEmpty()
+	text: string;
+
+	@ApiProperty()
+	@IsString()
+	@IsNotEmpty()
+	project_name: string;
+}
---
+++ src/reviews/entities/review.entity.ts
@@ -0,0 +1,34 @@
+import {
+	PrimaryGeneratedColumn,
+	Column,
+	CreateDateColumn,
+	UpdateDateColumn,
+} from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { Entity } from 'typeorm';
+
+@Entity()
+export class Review {
+	@ApiProperty()
+	@PrimaryGeneratedColumn('uuid')
+	id: string;
+
+	@ApiProperty()
+	@Column('varchar')
+	author: string;
+
+	@ApiProperty()
+	@Column('varchar')
+	text: string;
+
+	@ApiProperty()
+	@CreateDateColumn()
+	createdAt: Date;
+	@ApiProperty()
+	@UpdateDateColumn()
+	updatedAt: Date;
+
+	@ApiProperty()
+	@Column()
+	project_name?: string;
+}
