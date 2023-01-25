405b0091220e24970235831b680973586f19d62a
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-01-25 10:47:51 +0300

    feat: настроил проект для разработки

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+  const PORT = config.get<number>('API_PORT') ?? 5000;
+
+  app.setGlobalPrefix('/api/v1');
+  app.enableCors({ credentials: true, origin: true });
+
+  const apiDocument = new DocumentBuilder()
+    .setTitle('Piybeep API')
+    .setVersion('1.0')
+    .addBearerAuth({ type: 'http' })
+    .build();
+  const document = SwaggerModule.createDocument(app, apiDocument);
+  SwaggerModule.setup('/api/doc', app, document);
+
+  await app.listen(PORT);
 }
+
 bootstrap();
