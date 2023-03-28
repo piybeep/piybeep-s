fb9830b08e96ba00b7f0377316e8dc718fa0478a
ChocolateHokage <92468335+ChocolateHokage@users.noreply.github.com>
2023-03-28 12:47:53 +0300

    Файлы (#15)
    * add: files deleting
    
    * fix: files variables

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -3,6 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { File } from './entities/file.entity';
 import { Repository } from 'typeorm';
 import { FindAllFilesDto } from './dto/findAll-files.dto';
+import { remove } from 'fs-extra';
+import { join } from 'path';
 
 @Injectable()
 export class FilesService {
@@ -22,5 +24,22 @@ export class FilesService {
 		const skip = options.offset ?? 0;
 
 		return this.fileRepos.findAndCount({ take, skip });
+	}
+
+	async deleteFromDisk(path: string): Promise<void> {
+		return await remove(path);
+	}
+
+	async deleteFromDB(fileId: string): Promise<void> {
+		try {
+			const file = await this.fileRepos.findOneBy({ id: fileId });
+			await this.fileRepos.delete({ id: fileId });
+			await this.deleteFromDisk(
+				join(__dirname, '..', 'static', file.name),
+			);
+			return;
+		} catch (err) {
+			return err;
+		}
 	}
 }
