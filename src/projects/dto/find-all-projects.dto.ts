e98a7f5cc52725fe9c52d8de74ce197f3ecba893
ChocolateHokage <92468335+ChocolateHokage@users.noreply.github.com>
2023-03-28 12:03:33 +0300

    Проекты (#11)
    * add: projects crud
    
    * fix:  unused variable
    
    * fix: requested changes
    
    * some fixes

---
+++ src/projects/dto/find-all-projects.dto.ts
@@ -0,0 +1,11 @@
+import { IsInt, IsPositive } from "class-validator"
+
+export class FindAllProjectsDto{
+    @IsInt()
+    @IsPositive()
+    take: number
+    
+    @IsInt()
+    @IsPositive()
+    skip: number
+}
---
+++ src/projects/projects.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateProjectDto } from './dto/create-project.dto';
+import { UpdateProjectDto } from './dto/update-project.dto';
+import { Project } from './entities/project.entity';
+
+@Injectable()
+export class ProjectsService {
+	constructor(
+		@InjectRepository(Project)
+		private projectRepository: Repository<Project>,
+	) {}
+
+	async create(createProjectDto: CreateProjectDto) {
+		try {
+			const created_project =
+				this.projectRepository.save(createProjectDto);
+			return created_project;
+		} catch (err) {
+			console.log('project.service.create', err);
+			throw new Error('Server error');
+		}
+	}
+
+	findAll(take: number, skip: number) {
+		return this.projectRepository.findAndCount({take, skip});
+	}
+
+	findOne(id: string) {
+		return this.projectRepository.findOneBy({ id });
+	}
+
+	update(id: string, updateProjectDto: UpdateProjectDto) {
+		return this.projectRepository.update({ id }, updateProjectDto);
+	}
+
+	remove(id: string) {
+    return this.projectRepository.delete({id})
+  }
+}
